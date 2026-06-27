dc7bad9928f346596690c6f5d1f15a32ffab4370
shahadat-hossain99 <shahadathossain31500@gmail.com>
2026-06-27 19:26:30 +0600

    Successfully deployed in vercel and got the domain to use in client side

--- index.js
+++ index.js
@@ -31,7 +31,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const database = client.db(DB);
     const usersCollection = database.collection("users");
@@ -1262,7 +1262,7 @@ async function run() {
     });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
     console.log(
       "Pinged your deployment. You successfully connected to MongoDB!",
     );
